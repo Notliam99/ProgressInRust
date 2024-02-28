e6e1cf624d57ae5a748e60455f63e3d3e77751a5
Liam Tietjens <l.tietjens@hotmail.com>
2024-02-28 20:34:09 +1300

    needs word but the fundementials are there it can store and print grades ish :)

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
-use crossterm::{
-    execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
-};
+// use crossterm::{
+//     execute,
+//     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+// };
 
-use std::io::{stdin, stdout};
+use std::io::{stdin, stdout, Write};
 
 mod student;
 
 fn main() {
-    execute!(stdout(), EnterAlternateScreen)
-        .expect("Uncaught Error: Couldnot Leave Alternate Screen");
+    // execute!(stdout(), EnterAlternateScreen)
+    //     .expect("Uncaught Error: Couldnot Leave Alternate Screen");
 
     println!("Hello, world!");
 
     let mut input: String = String::new();
 
+    stdout().flush().expect("Error: Couldnot flush term output");
+
     stdin()
         .read_line(&mut input)
-        .expect("Uncaught Error: Cant Read Line From Console");
+        .expect("Error: Cant Read Line From Console");
 
     let mut student_liam: student::Student = student::Student::new();
 
-    execute!(stdout(), LeaveAlternateScreen)
-        .expect("Uncaught Error: Couldnot Leave Alternate Screen");
+    student_liam.add_assesment("wow", 50);
+    student_liam.add_assesment("wow", 50);
+
+    let results = student_liam.get_grades();
+
+    println!("\n\n{:?}", student_liam);
+
+    // execute!(stdout(), LeaveAlternateScreen)
+    //     .expect("Uncaught Error: Couldnot Leave Alternate Screen");
 }
--- src/student/mod.rs
+++ src/student/mod.rs
@@ -1,5 +1,4 @@
-// use std::option;
-
+use std::fmt;
 mod grade;
 
 pub struct Student {
@@ -23,14 +22,35 @@ impl Student {
     }
 
     pub fn add_assesment(&mut self, assesment_name: &'static str, score: u8) {
-        let mut grade: grade::Grades = grade::Grades::new();
-        grade.set(score, assesment_name).unwrap();
+        let grade: grade::Grades = grade::Grades {
+            assesment_name: assesment_name,
+            score: score,
+        };
 
         self.grades.push(grade);
     }
-    //
-    // pub fn get_grades(&mut self) -> Vec<grade::Grades> {
-    //     let return_val: Vec<grade::Grades>;
-    //     return_val
-    // }
+
+    pub fn get_grades(&self) -> Vec<(&'static str, u8)> {
+        let mut grades: Vec<(&'static str, u8)> = Vec::new();
+
+        for i in &self.grades {
+            let value = i.get();
+
+            grades.push(value);
+        }
+
+        grades
+    }
 }
+
+// yet to implement the display method so cant be printed still geting my head arround this :)
+
+// impl fmt::Display for Student {
+//     fn fmt(&self, f: fmt::Formatter) -> fmt::Result {
+//         write!(
+//             f,
+//             "(names: {:?}, email: {}, grades: {:?})",
+//             self.names, self.email, self.grades
+//         )
+//     }
+// }

db772cf8927900fece16419a15bd86413e5878b2
Liam Tietjens <l.tietjens@hotmail.com>
2024-03-04 20:31:11 +1300

    works !!! yepie yayay

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,19 @@ fn names() -> Vec<String> {
     for i in &mut names {
         print!("Enter Your {} Bellow\n\n> ", i);
 
+        *i = String::new();
+
         stdout().flush().expect("Error: Couldnot flush term output");
 
         stdin()
             .read_line(&mut *i)
             .expect("Error: Cant Read Line From Console");
 
-        *i = i.trim().to_string()
+        *i = i.trim().to_string();
+
+        if i == "" {
+            *i = "Nothing Entered".to_string()
+        }
     }
 
     names
@@ -38,32 +44,40 @@ fn add_grade() -> Grades {
     print!("Enter the name of the assesment\n\n> ");
     stdout().flush().expect("Error: Couldnot flush term output");
 
-    let mut assesment_name: String = String::from("No Name Was Entered");
+    let mut assesment_name: String = String::new();
 
     stdin()
         .read_line(&mut assesment_name)
         .expect("Error Cant Read Line From Console");
 
-    let mut assesment_score_temp: i64 = 111;
-    let mut assesment_score: String = String::new();
+    assesment_name = assesment_name.trim().to_string();
 
-    while { 0 > assesment_score_temp } && { assesment_score_temp > 100 } {
-        assesment_score = String::new();
+    if assesment_name == "" {
+        assesment_name = "No Name Was Set".to_string()
+    }
+
+    let mut assesment_score: i64 = 111;
+    let mut _assesment_score_string: String = String::new();
+
+    while assesment_score < 0 || assesment_score > 100 {
+        _assesment_score_string = String::new();
 
         print!("Enter the Score For The assesment\nScore Must Be Between 0 - 100\n\n> ");
 
         stdout().flush().expect("Error: Couldnot flush term output");
 
         stdin()
-            .read_line(&mut assesment_score)
+            .read_line(&mut _assesment_score_string)
             .expect("Error Cant Read Line From Console");
 
-        assesment_score_temp = assesment_score.parse().unwrap();
+        println!("{assesment_score}");
+
+        assesment_score = _assesment_score_string.trim().parse().unwrap_or(111);
     }
 
     Grades {
         assesment_name: assesment_name,
-        score: assesment_score.parse().unwrap(),
+        score: assesment_score as u8,
     }
 }
 
--- tests/mod.rs
+++ tests/mod.rs
@@ -3,10 +3,10 @@ use progress_in_rust::{student, student::grade};
 #[test]
 fn test_student_struct() {
     let student_var: student::Student = student::Student {
-        names: vec!["0", "1", "2"],
+        names: vec!["0".to_string(), "1".to_string(), "2".to_string()],
         email: "1",
         grades: vec![grade::Grades {
-            assesment_name: "0",
+            assesment_name: "0".to_string(),
             score: 0,
         }],
     };
